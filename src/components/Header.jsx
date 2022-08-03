80d66032b1546612f8ebf08ea748a8bedebacd0e
PauliVr <paulette.vr.10@gmail.com>
2022-08-03 11:10:38 -0500

    Change the Header components and add styles with TailWind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 function Header() {
-  return <h1 className='text-4xl font-bold uppercase'>Desde Header</h1>;
+  return (
+    <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
+      Seguimiento Pacientes <span className='text-indigo-600'>Veterinaria</span>
+    </h1>
+  );
 }
 
 export default Header;
