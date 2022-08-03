b8043cc90a66aa40502807d2225735c6b73cf675
PauliVr <paulette.vr.10@gmail.com>
2022-08-03 11:11:18 -0500

    Change the Form component, add the structure of the Form and add styles with TailWind

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,77 @@
 const Formulario = () => {
-  return <h2>Formulario</h2>;
+  return (
+    <div className='md:w-1/2 lg:w-2/5'>
+      <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes </h2>
+      <p className='text-lg mt-5 text-center mb-10'>
+        Añade Pacientes y <span className='text-indigo-600 font-bold'>Administralos</span>
+      </p>
+      <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+        <div className='mb-5'>
+          <label htmlFor='mascota' className='block text-gray-700 font-bold uppercase'>
+            Nombre Mascota
+          </label>
+          <input
+            id='mascota'
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            type='text'
+            placeholder='Nombre de la Mascota'
+          />
+        </div>
+
+        <div className='mb-5'>
+          <label htmlFor='propietario' className='block text-gray-700 font-bold uppercase'>
+            Nombre Propietario
+          </label>
+          <input
+            id='propietario'
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            type='text'
+            placeholder='Nombre del Propietario'
+          />
+        </div>
+
+        <div className='mb-5'>
+          <label htmlFor='email' className='block text-gray-700 font-bold uppercase'>
+            Email
+          </label>
+          <input
+            id='email'
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            type='email'
+            placeholder='Email contacto propietario'
+          />
+        </div>
+
+        <div className='mb-5'>
+          <label htmlFor='alta' className='block text-gray-700 font-bold uppercase'>
+            Alta
+          </label>
+          <input
+            id='alta'
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            type='date'
+          />
+        </div>
+
+        <div className='mb-5'>
+          <label htmlFor='sintomas' className='block text-gray-700 font-bold uppercase'>
+            Alta
+          </label>
+          <textarea
+            placeholder='Describe los Síntomas'
+            id='sintomas'
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+          />
+        </div>
+
+        <input
+          type='submit'
+          className='bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all'
+          value='Agregar Paciente'
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Formulario;
