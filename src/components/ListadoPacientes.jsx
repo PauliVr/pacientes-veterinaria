2a6703b0509710da6ca148f2c253324c01dedb92
PauliVr <paulette.vr.10@gmail.com>
2022-08-03 11:29:05 -0500

    Changes the component, import the Patient component and adds the styles with TailWind

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,5 +1,18 @@
+import Paciente from './Paciente';
+
 const ListadoPacientes = () => {
-  return <h2>Listado Pacientes</h2>;
+  return (
+    <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
+      <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+      <p className='text-lg mt-5 mb-10 text-center'>
+        Administra tus <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+      </p>
+      <Paciente />
+      <Paciente />
+      <Paciente />
+      <Paciente />
+    </div>
+  );
 };
 
 export default ListadoPacientes;
