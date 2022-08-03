ddbacce97bbe0e61a227e9babbd16f5593f22858
PauliVr <paulette.vr.10@gmail.com>
2022-08-03 11:09:05 -0500

    Changes the structure of the App file and adds styles with TailWind

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import ListadoPacientes from './components/ListadoPacientes';
 
 function App() {
   return (
-    <>
+    <div className='container mx-auto mt-20'>
       <Header />
-      <Formulario />
-      <ListadoPacientes />
-    </>
+      <div className='mt-12 md:flex'>
+        <Formulario />
+        <ListadoPacientes />
+      </div>
+    </div>
   );
 }
 
