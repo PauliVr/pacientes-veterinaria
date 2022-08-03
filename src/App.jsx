4ce656b846cce1e37d45ad755fe3da33a3409e0f
PauliVr <paulette.vr.10@gmail.com>
2022-08-02 21:22:26 -0500

    Import the components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoPacientes from './components/ListadoPacientes';
 
 function App() {
-
   return (
-    <div className='App'>
-      <h1>Hola Mundo</h1>
-    </div>
+    <>
+      <Header />
+      <Formulario />
+      <ListadoPacientes />
+    </>
   );
 }
 
