7f847b874a23c8533669a997551474ab23b8a59c
PauliVr <paulette.vr.10@gmail.com>
2022-08-02 22:41:00 -0500

    Install TailwindCSS, postCSS and autoprefixer as Dev dependencies and makes the config file for Tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 function Header() {
-  return <h1>Desde Header</h1>;
+  return <h1 className='text-4xl font-bold uppercase'>Desde Header</h1>;
 }
 
 export default Header;
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.jsx'], //el ** para todos los archivos que encuentre  el *.jsx indica que sea solo a los archivos con esa extension
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
