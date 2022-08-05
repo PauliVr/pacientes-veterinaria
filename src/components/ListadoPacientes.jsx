ed75a013bee4b207552c639334daa02916f89552
PauliVr <paulette.vr.10@gmail.com>
2022-08-04 19:33:53 -0500

    Render every patient in the patients list component

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,16 +1,30 @@
 import Paciente from './Paciente';
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({ pacientes }) => {
+  console.log(pacientes);
+
   return (
     <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
-      <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
-      <p className='text-lg mt-5 mb-10 text-center'>
-        Administra tus <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
-      </p>
-      <Paciente />
-      <Paciente />
-      <Paciente />
-      <Paciente />
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+          <p className='text-lg mt-5 mb-10 text-center'>
+            Administra tus <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+          </p>
+
+          {pacientes.map((paciente) => (
+            <Paciente key={paciente.id} paciente={paciente} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className='font-black text-3xl text-center'>No Hay Pacientes</h2>
+          <p className='text-lg mt-5 mb-10 text-center'>
+            Comienza agregando pacientes{' '}
+            <span className='text-indigo-600 font-bold'>Y aparecerán en este lugar</span>
+          </p>
+        </>
+      )}
     </div>
   );
 };
