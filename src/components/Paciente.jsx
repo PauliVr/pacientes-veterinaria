5dce67c69d56029f9840c83dd1b11f276d2ef464
PauliVr <paulette.vr.10@gmail.com>
2022-08-04 19:34:51 -0500

    Destructure props and adds into the jsx code

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,26 +1,22 @@
-const Paciente = () => {
+const Paciente = ({ paciente }) => {
+  const { nombre, propietario, email, fecha, sintomas } = paciente;
   return (
     <div className='mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl'>
       <p className='font-bold mb-3 text-gray-700 uppercase'>
-        Nombre: <span className='font-normal normal-case'>Hook</span>
+        Nombre: <span className='font-normal normal-case'>{nombre}</span>
       </p>
       <p className='font-bold mb-3 text-gray-700 uppercase'>
-        Nombre Propietario: <span className='font-normal normal-case'>Paulette</span>
+        Nombre Propietario: <span className='font-normal normal-case'>{propietario}</span>
       </p>
       <p className='font-bold mb-3 text-gray-700 uppercase'>
-        Email: <span className='font-normal normal-case'>correo@correo.com</span>
+        Email: <span className='font-normal normal-case'>{email}</span>
       </p>
       <p className='font-bold mb-3 text-gray-700 uppercase'>
-        Fecha Alta: <span className='font-normal normal-case'>10 Octubre 2022</span>
+        Fecha Alta: <span className='font-normal normal-case'>{fecha}</span>
       </p>
 
       <p className='font-bold mb-3 text-gray-700 uppercase'>
-        Síntomas:{' '}
-        <span className='font-normal normal-case'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate sequi doloremque
-          magni. Voluptates aut quisquam laudantium unde illo dolore delectus necessitatibus, saepe
-          ipsum soluta consectetur veritatis accusamus ex tempore molestias?
-        </span>
+        Síntomas: <span className='font-normal normal-case'>{sintomas}</span>
       </p>
     </div>
   );
