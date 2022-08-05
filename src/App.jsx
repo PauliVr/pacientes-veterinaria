2c1e2eb2c73b88650e4383544ad76bd123568d56
PauliVr <paulette.vr.10@gmail.com>
2022-08-05 15:58:19 -0500

    Creates the patient state variable and past as props

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import { useState } from 'react';
 
 function App() {
   const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
 
   return (
     <div className='container mx-auto mt-20'>
       <Header />
       <div className='mt-12 md:flex'>
-        <Formulario pacientes={pacientes} setPacientes={setPacientes} />
-        <ListadoPacientes pacientes={pacientes} />
+        <Formulario pacientes={pacientes} setPacientes={setPacientes} paciente={paciente} setPaciente={setPaciente}/>
+        <ListadoPacientes pacientes={pacientes} setPaciente={setPaciente} />
       </div>
     </div>
   );
