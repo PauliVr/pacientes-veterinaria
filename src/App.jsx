243742f58a1f540163f37717fa45a6110fd03a75
PauliVr <paulette.vr.10@gmail.com>
2022-08-04 19:35:24 -0500

    Send props to patients list

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <div className='mt-12 md:flex'>
         <Formulario pacientes={pacientes} setPacientes={setPacientes} />
-        <ListadoPacientes />
+        <ListadoPacientes pacientes={pacientes} />
       </div>
     </div>
   );
